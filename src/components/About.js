00209e4aa2e3db0de02ba5cfce4ef9b8b35dee54
Verusins <115052147+Verusins@users.noreply.github.com>
2023-10-24 20:07:01 -0400

    Contact update (Tho it does not work)
    :(

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,11 @@ import profile from "../assets/Person2.png";
 function About() {
   return (
     <>
+      <p className="easter-egg">
+        How dare you check a portfolio on phone? Show respect to others! Umm...
+        well, the true reason is just because my design works so bad on phones
+        so please go to pc to check this page, please...... qwq
+      </p>
       <img src={profile} alt="profile-pic" class="profile"></img>
       <div className="headline">
         <h1 className="name">Sigmund Wang</h1>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,65 @@
 import "./Contact.css";
 
+import React, { useState } from "react";
+import { send } from "@emailjs/browser";
+
 function Contact() {
-  return <></>;
+  const [toSend, setToSend] = useState({
+    userName: "",
+    email: "",
+    message: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send("Service ID", "template ID", toSend, "USER ID")
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="contact-main">
+      <h2 className="contact-title">CONTACT</h2>
+      <div className="contact-wrapper">
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            className="input name"
+            name="user_name"
+            value={toSend.userName}
+            onChange={handleChange}
+          />
+          <span className="placeholder Name">your name</span>
+          <input
+            type="email"
+            className="input email"
+            name="user_email"
+            value={toSend.email}
+            onChange={handleChange}
+          />
+          <span className="placeholder Email">your email</span>
+          <textarea
+            type="textarea"
+            className="input message"
+            name="message"
+            value={toSend.message}
+            onChange={handleChange}
+          />
+          <span className="placeholder msg">your message</span>
+          <br />
+          <input type="submit" className="submit-button" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
